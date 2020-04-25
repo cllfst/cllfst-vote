59e3b887820325ac47a8b66480009fbacbacbc1a
Zied <26070035+zguesmi@users.noreply.github.com>
2020-04-25 11:46:30 +0200

    Expot directly

--- src/util/utils.js
+++ src/util/utils.js
@@ -4,52 +4,47 @@ const nodemailer = require('nodemailer')
 const randomstring = require('randomstring')
 const appEnv = require('./app-env')
 
-function isAuthorized(authorization) {
-    return authorization !== appEnv.adminPassword
-}
-
-function generateRandomString(length) {
-    if (!length) {
-        length = 64
-    }
-    return randomstring.generate(length);
-}
-
-function sendEmail(senderEmail, senderPassword, to, subject, body) {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: senderEmail,
-            pass: senderPassword
-        }
-    })
-    
-    const mailOptions = {
-        from: senderEmail,
-        to: to,
-        subject: subject,
-        text: body
-    }
-    
-    console.log(`Sending email [to:${to}]`)
-    transporter.sendMail(mailOptions, function(err, info) {
-        if (err) {
-            console.log(`Error sending email [to:${to}, error:${err.message}]`)
-        }
-    })
-}
-
-function isValidRole(role) {
-    return ['SG', 'IN', 'EX', 'MA', 'SP', "ME"].indexOf(role) !== -1
-}
-
-// function isEmpty(object) {
-//     return JSON.stringify(object) == JSON.stringify({})
-// }
-
 module.exports = {
-    generateRandomString,
-    isValidRole,
-    sendEmail,
-    // isEmpty
+    isAdmin: (authorization) => {
+        return authorization !== appEnv.adminPassword
+    },
+    
+    generateRandomString: (length) => {
+        if (!length) {
+            length = 64
+        }
+        return randomstring.generate(length);
+    },
+    
+    sendEmail: (senderEmail, senderPassword, to, subject, body) => {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: senderEmail,
+                pass: senderPassword
+            }
+        })
+        
+        const mailOptions = {
+            from: senderEmail,
+            to: to,
+            subject: subject,
+            text: body
+        }
+        
+        console.log(`Sending email [to:${to}]`)
+        transporter.sendMail(mailOptions, function(err, info) {
+            if (err) {
+                console.log(`Error sending email [to:${to}, error:${err.message}]`)
+            }
+        })
+    },
+    
+    isValidRole: (role) => {
+        return ['SG', 'IN', 'EX', 'MA', 'SP', "ME"].indexOf(role) !== -1
+    },
+    
+    // function isEmpty(object) {
+    //     return JSON.stringify(object) == JSON.stringify({})
+    // }
 }
